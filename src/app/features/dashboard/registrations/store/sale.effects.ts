5a3ce82a62a5b11dc5debb930be2081f8e8a352c
Matias Gianni <matiasbostero1999@gmail.com>
2024-12-09 14:08:44 -0300

    arreglos en registrar

--- src/app/features/dashboard/registrations/store/sale.effects.ts
+++ src/app/features/dashboard/registrations/store/sale.effects.ts
@@ -6,116 +6,115 @@ import { SaleActions } from './sale.actions';
 import { RegistrationService } from '../../../../core/services/registrations.service';
 import { UsersService } from '../../../../core/services/users.service';
 import { CoursesService } from '../../../../core/services/courses.service';
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class SaleEffects {
+
+  loadRegistrations$:Actions<Action<string>>;
+  loadOptions$:Actions<Action<string>>;
+  createRegistration$:Actions<Action<string>>;
+  reloadAfterCreate$:Actions<Action<string>>;
+  deleteRegistration$:Actions<Action<string>>;
+  updateRegistration$:Actions<Action<string>>;
   constructor(
     private actions$: Actions,
     private registrationService: RegistrationService,
     private userService: UsersService,
     private courseService: CoursesService
-  ) {}
+  ) {
+    this.loadRegistrations$ = createEffect(() =>{
+      return this.actions$.pipe(
+         ofType(SaleActions.loadRegistrations),
+         concatMap(() =>
+           this.registrationService.getRegistration().pipe(
+             map((response) => SaleActions.loadRegistrationsSuccess({ data: response })),
+             catchError((error) => of(SaleActions.loadRegistrationsFailure({ error })))
+           )
+         )
+       )
+   });
 
-
-  loadRegistrations$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(SaleActions.loadRegistrations),
-      concatMap(() =>
-        this.registrationService.getRegistration().pipe(
-          map((data) => SaleActions.loadRegistrationsSuccess({ data })),
-          catchError((error) => of(SaleActions.loadRegistrationsFailure({ error })))
-        )
-      )
-    )
-  );
-
-
-  loadOptions$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(SaleActions.loadOptions),
-      concatMap(() =>
-        forkJoin({
-          courses: this.courseService.getCourses(),
-          users: this.userService.getUsers(),
-        }).pipe(
-          map(({ courses, users }) =>
-            SaleActions.loadOptionsSuccess({ courses, users })
-          ),
-          catchError((error) =>
-            of(SaleActions.loadOptionsFailure({ error }))
+    this.loadOptions$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(SaleActions.loadOptions),
+        concatMap(() =>
+          forkJoin({
+            courses: this.courseService.getCourses(),
+            users: this.userService.getUsers(),
+          }).pipe(
+            map(({ courses, users }) =>
+              SaleActions.loadOptionsSuccess({ courses, users })
+            ),
+            catchError((error) =>
+              of(SaleActions.loadOptionsFailure({ error }))
+            )
           )
         )
       )
-    )
-  );
+    );
 
-
-  createRegistration$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(SaleActions.createRegistration),
-      concatMap((action) => 
-        this.registrationService.createSale({
-          id: action.id,           
-          courseId: action.courseId, 
-          userId: action.userId     
-        }).pipe(
-          map((data) =>
-            SaleActions.createRegistrationSuccess({ data }) 
-          ),
-          catchError((error) =>
-            of(SaleActions.createRegistrationFailure({ error })) 
+    this.createRegistration$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(SaleActions.createRegistration),
+        concatMap((action) => 
+          this.registrationService.createSale({
+            id: action.id,           
+            courseId: action.courseId, 
+            userId: action.userId     
+          }).pipe(
+            map((data) =>
+              SaleActions.createRegistrationSuccess({ data }) 
+            ),
+            catchError((error) =>
+              of(SaleActions.createRegistrationFailure({ error })) 
+            )
           )
         )
       )
-    )
-  );
+    );
 
- 
-  reloadAfterCreate$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(SaleActions.createRegistrationSuccess),
-      map(() => SaleActions.loadRegistrations())
-    )
-  );
+    this.reloadAfterCreate$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(SaleActions.createRegistrationSuccess),
+        map(() => SaleActions.loadRegistrations())
+      )
+    );
 
-
-  deleteRegistration$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(SaleActions.deleteRegistration),
-      concatMap((action) =>
-        this.registrationService.deleteRegistration(action.id).pipe(
-          map(() => SaleActions.deleteRegistrationSuccess()),
-          catchError((error) =>
-            of(SaleActions.deleteRegistrationFailure({ error }))
+    this.deleteRegistration$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(SaleActions.deleteRegistration),
+        concatMap((action) =>
+          this.registrationService.deleteRegistration(action.id).pipe(
+            map(() => SaleActions.deleteRegistrationSuccess()),
+            catchError((error) =>
+              of(SaleActions.deleteRegistrationFailure({ error }))
+            )
           )
         )
       )
-    )
-  );
+    );
 
-  
-  updateRegistration$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(SaleActions.updateRegistration),
-     
-      concatMap((action) => {
+    this.updateRegistration$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(SaleActions.updateRegistration),
        
-        return this.registrationService
-          .updateRegistration(action.id, action.registration)
-          .pipe(
-            map(() => {
-          
-              return SaleActions.updateRegistrationSuccess();
-            }),
-            catchError((error) => {
-              console.error('Error occurred:', error); 
-              return of(SaleActions.updateRegistrationFailure({ error }));
-            })
-          );
-      })
-    )
-  );
-  
-  
-  
+        concatMap((action) => {
+         
+          return this.registrationService
+            .updateRegistration(action.id, action.registration)
+            .pipe(
+              map(() => {
+            
+                return SaleActions.updateRegistrationSuccess();
+              }),
+              catchError((error) => {
+                console.error('Error occurred:', error); 
+                return of(SaleActions.updateRegistrationFailure({ error }));
+              })
+            );
+        })
+      )
+    );
+  }
 }
